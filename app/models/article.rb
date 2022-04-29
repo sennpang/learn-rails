cc3103a19a6ef5f2b6490aa1bbc06e8c2903712a
yongzs <673262625@qq.com>
2022-04-29 12:13:49 +0800

    add status

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,11 @@
 class Article
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Visible
   has_many :comments
   field :title, type: String
   field :text, type: String
+  field :status, type: String
   validates :title, presence: true, length: { minimum: 3 }
   validates :text, presence: true, length: { minimum: 10 }
-  VALID_STATUSES = %w[public private archived]
-  validates :status, inclusion: { in: VALID_STATUSES }
-  def archived?
-    status == 'archived'
-  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,8 @@
 class Comment
+  include Visible
   include Mongoid::Document
   include Mongoid::Timestamps
   field :commenter, type: String
   field :body, type: String
   belongs_to :article
-  VALID_STATUSES = %w[public private archived]
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
