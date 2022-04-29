acecdea76a65a4d18896002a216f1d6247ce48c6
yongzs <673262625@qq.com>
2022-04-29 11:33:41 +0800

    add status

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,9 @@ class Article
   field :text, type: String
   validates :title, presence: true, length: { minimum: 3 }
   validates :text, presence: true, length: { minimum: 10 }
+  VALID_STATUSES = %w[public private archived]
+  validates :status, inclusion: { in: VALID_STATUSES }
+  def archived?
+    status == 'archived'
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,11 @@ class Comment
   field :commenter, type: String
   field :body, type: String
   belongs_to :article
+  VALID_STATUSES = %w[public private archived]
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
